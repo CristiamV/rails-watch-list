19839742f48775ddc64bdc41d2edad49e7cd19e7
Cristiam Vasquez <cristiamvasquez@gmail.com>
2021-10-22 13:39:07 -0400

    Añadio reviews

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -7,6 +7,7 @@ class ListsController < ApplicationController
   def show
     @list = List.find(params[:id])
     @bookmarks = Bookmark.where(list: @list)
+    @review = Review.new(list: @list)
     
   end
 
--- db/migrate/20211022161757_create_reviews.rb
+++ db/migrate/20211022161757_create_reviews.rb
@@ -1,6 +1,6 @@
 class CreateReviews < ActiveRecord::Migration[6.1]
   def change
-    create_table :reviews do |t|
+    create_table :reviews do |t|      
       t.references :list, null: false, foreign_key: true
 
       t.timestamps
